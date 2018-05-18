1071e2d05f8ff19e327503b7737d599158a78ef2
Pratibha Shrivastav <35465871+PratibhaShrivastav@users.noreply.github.com>
2018-05-19 01:00:28 +0530

    Create jsc.js

---
+++ jsc.js
@@ -0,0 +1,11 @@
+var pass=document.getElementById("password").value;
+var pass1=document.getElementById("password1").value;
+function check(){
+if(pass==pass1){
+  document.getElementById("button").disabled=false;
+  alert("jsshfvdrk");
+}
+else {
+  document.getElementById("button").disabled=true;
+}
+}
